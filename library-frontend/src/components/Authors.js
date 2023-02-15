861b41e994488e0b3cc6e1d06efee86d3e27b629
RotcivL <victor.lee.98.vic@gmail.com>
2023-02-15 13:46:16 +1100

    Ex.8.11

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,4 +1,21 @@
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS_BOOKS, EDIT_YEAR } from '../queries'
 const Authors = ({ authors }) => {
+  const [name, setName] = useState('')
+  const [year, setYear] = useState('')
+
+  const [updatedAuthor] = useMutation(EDIT_YEAR, {
+    refetchQueries: [{ query: ALL_AUTHORS_BOOKS }],
+  })
+
+  const submit = async (event) => {
+    event.preventDefault()
+    updatedAuthor({ variables: { name, setBornTo: Number(year) } })
+    setName('')
+    setYear('')
+  }
+
   return (
     <div>
       <h2>authors</h2>
@@ -18,6 +35,25 @@ const Authors = ({ authors }) => {
           ))}
         </tbody>
       </table>
+
+      <h2>Set Birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          boirn
+          <input
+            value={year}
+            onChange={({ target }) => setYear(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -38,3 +38,14 @@ export const CREATE_BOOK = gql`
     }
   }
 `
+
+export const EDIT_YEAR = gql`
+  mutation editYear($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`
